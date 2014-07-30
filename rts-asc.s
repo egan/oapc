4afbcb113867278bfe62c35e9f8d54a857b4f6e8
Egan McComb <egan.mccomb@gmail.com>
2014-07-30 09:18:25 -0700

    Added vim modeline and deleted trailing whitespace.

--- rts-asc.s
+++ rts-asc.s
@@ -3,61 +3,61 @@
 ;variable defined in internal data ram starting from 4E Unit
 ;Stack bottom must be set to 70H in main program to prevent
 ;a confilict between assembly variables and stack space
- 
+
 ;50H, 51H current Up/Down counter Value-----------------> Xc(i) pulse
 XCHI epz 50H
 XCL epz 51H
- 
+
 ;52H, 53H interpolation position increment --------------> DFX Pulse
 DFXH epz 53H
 DFXL epz 52H
- 
+
 ;54H, 55H position following error --------> PEX(i) Pulse
 PEXH epz 54H
 PEXL epz 55H
- 
+
 ;56H, 57H ,58H,59H Residual distance ---------------> Fx(i) Pulse
 FX4 epz 59H
 FX3 epz 58H
 FX2 epz 57H
 FX1 epz 56H
- 
+
 ;5AH Proportioanl gain ---------------> Kp
 KP epz 5AH
- 
+
 ;5BH Servo locked Proportioanl gain ---------------> Kpinp
 KPINP epz 5BH
- 
+
 ; 5EH, 5FH Distance motion command per sampling period --------------> DX Pulse
 DXH epz 5EH
 DXL epz 5FH
- 
+
 ;60H,61H,62H,63H current absolute position counter-----------------------ABX Pusle
 ABSX4 epz 63H
 ABSX3 epz 62H
 ABSX2 epz 61H
 ABSX1 epz 60H
- 
+
 RDABSEN epz 64H
 INTCNT epz 65h
- 
+
 ;66H,67H Temporary variables
 DABSH epz 66h
 DABSL epz 67h
- 
+
 ;4CH,4DH Velocity Command Value-------------------------VELX
 VELXH epz 4CH
 VELXL epz 4DH
- 
+
 ; 4EH, 4FH Position increment/decrement -------------- delta Xc(i) Pulse
 DXCH epz 4EH
 DXCL epz 4FH
- 
+
 XGO def	021H.0 ; motion start/stop flag
 INPOS def	021H.1 ; used for servo lock flag
 MCDIR def	021H.4 ; direction command from PC
 DATSAV def	021H.3
- 
+
 MSIGN0 def	021H.5
 MSIGN1 def	021H.6
 MSIGNALL def	021H.7
@@ -66,10 +66,10 @@ BUFF_ADRH def 04BH
 _DA1_LOW equ 0FE18h
 _DA1_HIGH equ 0FE19h
 _DA_CNVT equ 0FE1Fh
- 
+
 _UDCNT1_LOW equ 0FF05h
 _UDCNT1_HIGH equ 0FF06h
- 
+
 ;***********************************************************************
 ; Timer interrupt Initial
 ;
@@ -81,7 +81,7 @@ push acc
 push b
 push dpl
 push dph
- 
+
 mov dptr, #0xFA1B	; set Timer1 interrupt vector
 mov a, #0x02	;ljump timer interrupt subroutine address
 movx @dptr, a
@@ -101,15 +101,15 @@ lcall DAOUT
 lcall INITVAR;
 setb tr1
 orl ie,#0x88
- 
+
 pop dph
 pop dpl
 pop b
 pop acc
 pop psw
 ret
- 
- 
+
+
 ;***********************************************************************
 ; Timer interrupt subroutine
 ;
@@ -117,16 +117,16 @@ ret
 org 0x9F50
 push B
 push acc
-orl psw, #0x18	
+orl psw, #0x18
 mov tl1, #0E5h	;20Mhz,3ms (EC78h),10ms(BEE5h),
 mov th1, #0BEh	;20Mhz,1ms F97DH
 push dpl
 push dph
- 
+
 ;mov dptr, #0xFF00
 ;mov a, #0xFF
 ;movx @dptr, a
- 
+
 lcall ReadUDCounter ;module 1
 lcall PosDiffCAL ;module 2
 lcall PEXUpdate ;module 3
@@ -134,11 +134,11 @@ lcall INPOSJudge ;module 4
 lcall	PPCAL ;module 5
 lcall	DAOUTVEL ;module 6
 lcall INTPAndFXCheck ;module 7,8
- 
+
 ;mov dptr, #0xFF00
 ;mov a, #0x00
 ;movx @dptr, a
- 
+
 done:
 pop dph
 pop dpl
@@ -146,8 +146,8 @@ pop acc
 pop B
 pop psw
 reti
- 
- 
+
+
 ;***********************************************************************
 ; Initial variables
 ;
@@ -171,25 +171,25 @@ mov INTCNT ,#00h
 mov RDABSEN,#00h
 mov DABSH ,#00h
 mov DABSL ,#00h
-ret	
+ret
 ;***********************************************************************
 ; Clear position counter
 ;Input:
 ;
 ;***********************************************************************
-ClearCounter:	
+ClearCounter:
 mov dptr, #_UDCNT1_HIGH ; Counter1
 mov a,#00h
 movx @dptr,a
 ret
- 
+
 ;***********************************************************************
 ; D/A output
 ;Input:
 ; High byte (B), Low byte (ACC)
 ;
 ;***********************************************************************
-DAOut:	
+DAOut:
 mov dptr, #_DA1_LOW
 movx @dptr,a
 nop
@@ -202,7 +202,7 @@ nop
 mov dptr, #_DA_CNVT ;start conversion
 movx @dptr,a
 ret
- 
+
 ;***********************************************************************
 ;1. Read Up/Down counter
 ;
@@ -223,7 +223,7 @@ mov dptr,#_UDCNT1_HIGH
 movx a, @dptr
 mov XCHI,a ;Read HIGH byte
 ret
- 
+
 ;***********************************************************************
 ;2. Motion increment/ decrement calculation and update absolute counter
 ;
@@ -243,13 +243,13 @@ ret
 ; ABSX1: Current absolute value 1ST byte
 ;
 ;***********************************************************************
-PosDiffCAL:	
+PosDiffCAL:
 mov r1,XCHI
 mov r0,XCL
 lcall CSUB16
 mov DXCH,r1
 mov DXCL,r0
- 
+
 mov r3,DABSH
 mov r2,DABSL
 lcall ADD16
@@ -263,23 +263,23 @@ ABXUpdate:
 mov r1,DABSH
 mov r0,DABSL
 lcall C16toC24 ;r0,r1,r2,r3
- 
+
 mov r7,ABSX4
 mov r6,ABSX3
 mov r5,ABSX2
 mov r4,ABSX1
- 
+
 lcall CADD24 ;calculate the absolute position
- 
+
 mov ABSX4,r3
 mov ABSX3,r2
 mov ABSX2,r1
 mov ABSX1,r0
- 
+
 mov DABSH,#00h
-mov DABSL,#00h	
+mov DABSL,#00h
 ret
- 
+
 ;***********************************************************************
 ;3. Position error update
 ;
@@ -296,7 +296,7 @@ ret
 ; PEXL: Position following error at this sampling time LOW byte
 ;
 ;***********************************************************************
-PEXUpdate:	
+PEXUpdate:
 mov r1,PEXH ;Last PEX
 mov r0,PEXL ;last PEX
 mov r3,DXCH
@@ -313,7 +313,7 @@ OutPEX:
 mov PEXH,r1
 mov PEXL,r0
 ret
- 
+
 ;***********************************************************************
 ;4. In-position Discrimination
 ;
@@ -325,17 +325,17 @@ ret
 ; INPOS: Servo locked flag at this sampling time
 ;
 ;***********************************************************************
-INPOSJudge:	
+INPOSJudge:
 jnb XGO, MCStop
 clr INPOS
 ret
-MCStop:	
+MCStop:
 jnb INPOS, SetINPOS
 ret
 SetINPOS:
 setb INPOS
 ret
- 
+
 ;***********************************************************************
 ;5. Proportional control algorithm calculation
 ;
@@ -363,7 +363,7 @@ lcall mul16
 mov VELXL, r0
 mov VELXH, r1
 ret
- 
+
 ;***********************************************************************
 ;6. D-to-A Conversion for velocity command output
 ;
@@ -374,7 +374,7 @@ ret
 ; VELDA: Digital Velocity command value
 ;
 ;***********************************************************************
-DAOUTVEL:	
+DAOUTVEL:
 jb MSIGNALL, VELNEG
 mov r0,#0FFh	;VELX is Positive
 mov r1,#07Fh
@@ -393,7 +393,7 @@ mov a,r0
 mov b,r1
 lcall DAOut
 ret
- 
+
 ;***********************************************************************
 ;7. Interpolation command for next interrupt and Final Position update
 ;
@@ -430,7 +430,7 @@ mov FX3,r2
 mov FX2,r1
 mov FX1,r0
 sjmp FXCheckXGO
- 
+
 ;***********************************************************************
 ;8. Interpolation end Discrimination and update distance¨Cto-go at final period
 ;
@@ -445,26 +445,26 @@ sjmp FXCheckXGO
 ; XGO: Motion start/stop flag;
 ;
 ;***********************************************************************
- 
-FXCheckXGO:	
+
+FXCheckXGO:
 mov a,FX3
 jb acc.7, CLRXGO ;FX is negative
 mov DXH, DFXH
 mov DXL, DFXL
 ret
- 
-CLRXGO:	
+
+CLRXGO:
 mov DXH,r5
 mov DXL,r4
 CLR XGO
-ret	
-StopINTP:	
-mov DXH,#000h	
-mov DXL,#000h	
-ret	
- 
- 
- 
+ret
+StopINTP:
+mov DXH,#000h
+mov DXL,#000h
+ret
+
+
+
 ;====================================================================
 ; subroutine ADD16
 ; 16-Bit Signed (2's Complement) Addition
@@ -477,7 +477,7 @@ ret
 ;
 ; alters: acc, C, OV
 ;====================================================================
- 
+
 ;org 8670
 ADD16:
 ; orl PSW, #0x18 ; Register Bank 3
@@ -490,7 +490,7 @@ mov r1, a ; save result in Z high byte
 ;mov C, OV
 clr C
 ret
- 
+
 ;====================================================================
 ; subroutine CSUB16
 ; 16-Bit Signed (2's Complement) Subtraction
@@ -531,7 +531,7 @@ ret
 ;====================================================================
 ;org 86e0
 MUL16:
- 
+
 orl PSW, #0x18 ; Register Bank 3
 clr MSIGNALL ;
 lcall Cr0r1 ; 2's comp -> Mag/Sign
@@ -540,7 +540,7 @@ lcall UMUL16
 lcall Mr0r3 ; Mag/Sign -> 2's Comp
 ;lcall FRound ;
 ret
- 
+
 ;====================================================================
 ; subroutine UMUL16
 ; 16-Bit x 16-Bit to 32-Bit Product Unsigned Multiply
@@ -593,7 +593,7 @@ pop 18H ; retrieve result low byte
 pop dpl
 pop B
 ret
- 
+
 ;===================================================================
 ; subroutine Cr0r1
 ; 16-Bit 2's Complement -> magnitude / Sign Bit Conversion
@@ -610,7 +610,7 @@ Cr0r1: mov a, r1 ; high byte into accumulator
 jb acc.7, c0a ; negative if bit 7 is 1
 clr MSIGN1 ; clear sign bit if 'positive'
 ret ; done
- 
+
 c0a: setb MSIGN1
 mov a, r0 ; number is negative
 cpl a ; complement
@@ -621,7 +621,7 @@ cpl a ; complement
 addc a, #0
 mov r1, a
 ret
- 
+
 ;====================================================================
 ; subroutine Cr2r3
 ; 16-Bit 2's Complement -> magnitude / Sign Bit Conversion
@@ -638,7 +638,7 @@ Cr2r3: mov a, r3 ; read high into accumulator
 jb acc.7, c1a ; negative if bit 7 is 1
 clr MSIGN0 ; clear sign bit if 'positive'
 ret ; done
- 
+
 c1a: setb MSIGN0 ; set sign flag
 mov a, r2 ; number is negative
 cpl a ; complement
@@ -660,7 +660,7 @@ ret
 ;
 ; alters: acc, C
 ;====================================================================
- 
+
 C2Comp:
 mov a, r0 ; number is negative
 cpl a ; complement
@@ -671,7 +671,7 @@ cpl a ; complement
 addc a, #0h0
 mov r1, a
 ret
- 
+
 ;====================================================================
 ; subroutine Mr0r3
 ; 32-Bit magnitude / Sign Bit -> 2's Complement Conversion
@@ -688,10 +688,10 @@ ret
 Mr0r3: jb MSIGN1 , Mr0r3b ; test X sign
 jb MSIGN0, Mr0r3a ; test Y sign
 ret
- 
+
 Mr0r3b: jnb MSIGN0, Mr0r3a
 ret
- 
+
 Mr0r3a:
 setb MSIGNALL ;
 ;mov a, r0 ; negate number
@@ -711,7 +711,7 @@ setb MSIGNALL ;
 ;addc a, #0
 ; mov r3, a
 ret ; done
- 
+
 ;====================================================================
 ;16-Bit 2's Complement -> 32-Bit 2's Complement Conversion
 ;SC16TOSC32
@@ -732,9 +732,9 @@ mov a,r1
 jnb acc.7, CC32End
 mov r3,#0FFh
 mov r2,#0FFh
-CC32End:	
+CC32End:
 ret
- 
+
 ;====================================================================
 ; subroutine CADD32
 ; 32-Bit Signed (2's Complement) Addition
@@ -749,7 +749,7 @@ ret
 ;
 ; alters: acc, C, OV
 ;====================================================================
- 
+
 ;org 8670
 CADD32:
 orl PSW, #0x18 ; Register Bank 3
@@ -757,21 +757,21 @@ clr C ; clear carry flag
 mov a, r0 ; load X low byte into acc
 addc a, r4 ; add Y low byte
 mov r0, a ; put result in Z low byte
- 
+
 mov a, r1 ; load X low byte into acc
 addc a, r5 ; add Y low byte
 mov r1, a ; put result in Z low byte
- 
+
 mov a, r2 ; load X low byte into acc
 addc a, r6 ; add Y low byte
 mov r2, a ; put result in Z low byte
- 
+
 mov a, r3 ; load X low byte into acc
 addc a, r7 ; add Y low byte
 mov r3, a ; put result in Z low byte
- 
+
 mov C, OV
- 
+
 ret
 ;====================================================================
 ;====================================================================
@@ -788,31 +788,31 @@ ret
 ;
 ; alters: acc, C, OV
 ;====================================================================
- 
+
 ;org 8670
 CSUB32:
- 
+
 orl PSW, #0x18 ; Register Bank 3
 clr C ; clear carry flag
 mov a, r4 ; load X low byte into acc
 subb a, r0 ; add Y low byte
 mov r0, a ; put result in Z low byte
- 
+
 mov a, r5 ; load X low byte into acc
 subb a, r1 ; add Y low byte
 mov r1, a ; put result in Z low byte
- 
+
 mov a, r6 ; load X low byte into acc
 subb a, r2 ; add Y low byte
 mov r2, a ; put result in Z low byte
- 
+
 mov a, r7 ; load X low byte into acc
 subb a, r3 ; add Y low byte
 mov r3, a ; put result in Z low byte
- 
+
 mov C, OV
- 
-ret	
+
+ret
 ;====================================================================
 ;16-Bit 2's Complement -> 24-Bit 2's Complement Conversion
 ;SC16TOC24
@@ -827,13 +827,13 @@ ret
 ;Alters: ACC, C
 ;====================================================================
 C16toC24:
-mov r2,#00h	
+mov r2,#00h
 mov a,r1
 jnb acc.7, CC24End
 mov r2,#0FFh
-CC24End:	
+CC24End:
 ret
- 
+
 ;====================================================================
 ; subroutine CADD24
 ; 24-Bit Signed (2's Complement) Addition
@@ -848,7 +848,7 @@ ret
 ;
 ; alters: acc, C, OV
 ;====================================================================
- 
+
 ;org 8670
 CADD24:
 orl PSW, #0x18 ; Register Bank 3
@@ -856,19 +856,19 @@ clr C ; clear carry flag
 mov a, r0 ; load X low byte into acc
 addc a, r4 ; add Y low byte
 mov r0, a ; put result in Z low byte
- 
+
 mov a, r1 ; load X low byte into acc
 addc a, r5 ; add Y low byte
 mov r1, a ; put result in Z low byte
- 
+
 mov a, r2 ; load X low byte into acc
 addc a, r6 ; add Y low byte
 mov r2, a ; put result in Z low byte
- 
+
 mov C, OV
- 
+
 ret
- 
+
 ;====================================================================
 ; subroutine CSUB24
 ; 24-Bit Signed (2's Complement) Subtract
@@ -883,27 +883,27 @@ ret
 ;
 ; alters: acc, C, OV
 ;====================================================================
- 
+
 CSUB24:
- 
+
 orl PSW, #0x18 ; Register Bank 3
 clr C ; clear carry flag
 mov a, r4 ; load X low byte into acc
 subb a, r0 ; add Y low byte
 mov r0, a ; put result in Z low byte
- 
+
 mov a, r5 ; load X low byte into acc
 subb a, r1 ; add Y low byte
 mov r1, a ; put result in Z low byte
- 
+
 mov a, r6 ; load X low byte into acc
 subb a, r2 ; add Y low byte
 mov r2, a ; put result in Z low byte
- 
+
 mov C, OV
- 
-ret	
- 
+
+ret
+
 ;====================================================================
 ; subroutine ROUND
 ; fixed float Signed (2's Complement) Round
@@ -918,15 +918,15 @@ ret
 ;
 ; alters: acc, C, OV
 ;====================================================================
- 
+
 ;org 8670
 FRound:
 orl PSW, #0x18 ; Register Bank 3
 clr C ; clear carry flag
 mov a,r3
- 
+
 jnb acc.7, FRPositive
- 
+
 mov a,r0 ; r0 > 0.5 (80h)
 jb acc.7, FREND
 mov a, r1
@@ -936,7 +936,7 @@ mov a,r2
 subb a,#00h
 mov r2,a
 sjmp FREND
-FRPositive:	
+FRPositive:
 mov a,r0 ; r0 > 0.5 (80h)
 jnb acc.7, FREND
 mov a, r1
@@ -945,8 +945,9 @@ mov r1,a
 mov a,r2
 addc a,#00h
 mov r2,a
- 
+
 FREND:
 clr c
-ret	
- 
+ret
+
+; vim: filetype=tasm
